687bc788f237c0af476717d0d566780b2d0551a1
toanbui626391 <toanbui626391@gmail.com>
2025-11-09 10:19:07 +0700

    updates

--- stack/generate_parentheses.py
+++ stack/generate_parentheses.py
@@ -1,23 +1,25 @@
-#problem understanding
-    #Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-
-#strategy to solve the problem
-    #using stack to build a stack of valid open and close parentheses
-    #variable
-        #stack
-        #res
-        #openN
-        #closeN
-#strategy to solve the problem v2:
-    #this is binary tree problem and not a stack problem
-    #we can use depth first search and crecursive call to sole this problem
-    #have to form valid parentheses string. Therefore number of open have to >=  number of close
-    #limit n pair of parentheses. Therefore number of open <= n
-
-    #variables:
-        # num_open (int): number of open parentheses
-        # num_close (int): number of close parentheses
-        #string (string): current string of the current node
+# problem understanding
+#     Given n pairs of parentheses, write a function to generate all
+#     combinations of well-formed parentheses.
+#
+# strategy to solve the problem
+#     using stack to build a stack of valid open and close parentheses
+#     variable
+#         stack
+#         res
+#         openN
+#         closeN
+# strategy to solve the problem v2:
+#     this is binary tree problem and not a stack problem
+#     we can use depth first search and crecursive call to sole this problem
+#     have to form valid parentheses string. Therefore number of open have to >=
+#     number of close
+#     limit n pair of parentheses. Therefore number of open <= n
+#
+#     variables:
+#         num_open (int): number of open parentheses
+#         num_close (int): number of close parentheses
+#         string (string): current string of the current node
 ###########################################################reference solution
 from typing import List
 class Solution:
