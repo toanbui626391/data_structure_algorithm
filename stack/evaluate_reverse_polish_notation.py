047ace39ff3dc2e2bf5ea5a552905be0bb32e889
toanbui626391 <toanbui626391@gmail.com>
2025-11-09 10:17:41 +0700

    updates

--- stack/evaluate_reverse_polish_notation.py
+++ stack/evaluate_reverse_polish_notation.py
@@ -1,19 +1,21 @@
-#problem understanding
-    #You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
-    #Evaluate the expression. Return an integer that represents the value of the expression.
-
-#strategy to solve the problem
-    #using stack because we can take two most recent number
-        #operation is do by sequence, result of the last operation will be append to stack and then continue
-    #check for operator character to do the operation
-    #if character is not operator just append number
-    #return is the last remaining number in the stack
-    #variable:
-        #stack to keep track of the two most recent numbers from tokes string
-
-    #why using stack?
-        #stack to keep current two value of every operation.
-
+# problem understanding
+#     You are given an array of strings tokens that represents an arithmetic
+#     expression in a Reverse Polish Notation.
+#     Evaluate the expression. Return an integer that represents the value of
+#     the expression.
+#
+# strategy to solve the problem
+#     using stack because we can take two most recent number
+#         operation is do by sequence, result of the last operation will be
+#         append to stack and then continue
+#     check for operator character to do the operation
+#     if character is not operator just append number
+#     return is the last remaining number in the stack
+#     variable:
+#         stack to keep track of the two most recent numbers from tokes string
+#
+#     why using stack?
+#         stack to keep current two value of every operation.
 
 
 ##########################################reference solution
