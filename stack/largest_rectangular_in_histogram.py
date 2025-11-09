6b3999d3bd77c911caf0f3c8d5a02a30b18a3226
toanbui626391 <toanbui626391@gmail.com>
2025-11-09 10:22:08 +0700

    updates

--- stack/largest_rectangular_in_histogram.py
+++ stack/largest_rectangular_in_histogram.py
@@ -1,12 +1,14 @@
-#problem understanding
-
-#stretegy to solve the proble
-    #maxArea (int): to hold the current max value
-    #stack (index, height): to hold stack of index and height value
-#why using stack
-    # stack (start, height): keep record of increase hights when we meet lower hight we can calculate area form by column of the pop stack
-    # max_area (int): to keep the current max area
-    # start = i: to keep the start index of col and 
+# problem understanding
+#
+# strategy to solve the problem
+#     maxArea (int): to hold the current max value
+#     stack (index, height): to hold stack of index and height value
+#
+# why using stack
+#     stack (start, height): keep record of increase hights when we meet lower
+#     hight we can calculate area form by column of the pop stack
+#     max_area (int): to keep the current max area
+#     start = i: to keep the start index of col and
 ##########################################reference solution
 from typing import List
 class Solution:
