f60629bcf0e367458171904a3b23edca1fac0465
toanbui626391 <toanbui626391@gmail.com>
2025-11-10 09:33:36 +0700

    updates

--- stack/largest_rectangular_in_histogram.py
+++ stack/largest_rectangular_in_histogram.py
@@ -27,3 +27,28 @@ class Solution:
         for i, h in stack:
             maxArea = max(maxArea, h * (len(heights) - i))
         return maxArea
+
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # Append a 0-height bar to force calculation of all remaining bars
+        # This acts as a sentinel
+        heights.append(0)
+        # The stack will store indices.
+        # We add -1 as a base sentinel to simplify width calculation.
+        stack = [-1]
+        max_area = 0
+        for i in range(len(heights)):
+            # We process bars when we find a bar shorter than the one 
+            # at the top of the stack.
+            # This new, shorter bar is the "right boundary".
+            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+                # This is the bar for which we are calculating the max area
+                h = heights[stack.pop()]
+                # stack[-1] is now the "left boundary"
+                # i is the "right boundary"
+                w = i - stack[-1] - 1
+                max_area = max(max_area, h * w)
+            # Push the current index onto the stack
+            stack.append(i)            
+        # Remove the sentinel bar we added (good practice, though not strictly needed)
+        heights.pop()        
+        return max_area          
