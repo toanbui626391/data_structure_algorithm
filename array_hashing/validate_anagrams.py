c0c8d954e490ce791759b905ac21370982479b07
toanbui626391 <toanbui626391@gmail.com>
2026-03-19 15:49:07 +0700

    update validate anagrams

--- array_hashing/validate_anagrams.py
+++ array_hashing/validate_anagrams.py
@@ -13,18 +13,30 @@ Constraints:
   Two anagrams must have the same length.
 """
 
-from collections import Counter
+
+def isAnagram(s: str, t: str) -> bool:
+    # Length mismatch means they cannot be anagrams.
+    if len(s) != len(t):
+        return False
+
+    # Count character frequencies manually using
+    # a plain dict instead of Counter.
+    freq_s: dict[str, int] = {}
+    freq_t: dict[str, int] = {}
+
+    for char in s:
+        freq_s[char] = freq_s.get(char, 0) + 1
+
+    for char in t:
+        freq_t[char] = freq_t.get(char, 0) + 1
+
+    # Equal frequency maps confirm anagram relationship.
+    return freq_s == freq_t
 
 
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # Length mismatch means they cannot be anagrams.
-        if len(s) != len(t):
-            return False
-        # Same character counts confirm anagram relationship.
-        return Counter(s) == Counter(t)
+if __name__ == "__main__":
+    # Expected: True
+    print(isAnagram("anagram", "nagaram"))
+
+    # Expected: False
+    print(isAnagram("rat", "car"))
