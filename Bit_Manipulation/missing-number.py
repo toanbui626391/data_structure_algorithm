2318d91ec05cf003dd72e16f98875c400af37433
toanbui626391 <toanbui626391@gmail.com>
2026-04-30 15:39:36 +0700

    update missing numbers

--- Bit_Manipulation/missing-number.py
+++ Bit_Manipulation/missing-number.py
@@ -15,12 +15,8 @@ from typing import List
 
 
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        # XOR properties: x^x=0, x^0=x, commutative.
-        count = len(nums)
-        result = 0
-        for i in range(1, count + 1):
-            result ^= i
-        for num in nums:
-            result ^= num
-        return result
+  def missingNumber(nums):
+      missing = len(nums)
+      for i, num in enumerate(nums):
+          missing ^= i ^ num
+      return missing
